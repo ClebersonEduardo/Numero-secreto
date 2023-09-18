5ca8c2c942ac17dc2884594b22b27f35e2eb5ab0
ClebersonEduardo <clebersoneduardo112@gmail.com>
2023-09-18 17:52:22 -0300

    Numero Secreto

---
+++ app/validacao.js
@@ -0,0 +1,45 @@
+function verificaSeOChutePossuiUmValorValido(chute){
+    const numero = +chute
+
+    if(chuteForInvalido(numero)){
+        elementoChute.innerHTML += '<div>Valor invalido</div>'
+        return
+    }
+
+    if(numeroForMaiorOuMenorQueONumeroPermitido(numero)){
+        elementoChute.innerHTML += `
+        <div>Numero inválido: Fale um número entre ${menorValor} 
+        e ${maiorValor}</div>
+        `
+        return
+    }
+
+    if(numero === numeroSecreto){
+        document.body.innerHTML = `
+        <h2>Você Acertou!</h2> 
+        <h3>O número secreto era ${numeroSecreto}</h3>
+        <button id="jogar-novamente" class="btn-jogar">Jogar Novamente</button>
+        `
+
+    }else if (numero > numeroSecreto) {
+        elementoChute.innerHTML += `<div>O número secreto é menor <i class="fa-solid fa-arrow-down"></i></div>`
+    }else {
+        elementoChute.innerHTML += `<div>O número secreto é maior <i class="fa-solid fa-arrow-up"></i></div>`
+    }
+
+}
+
+function chuteForInvalido(numero) {
+    return Number.isNaN(numero)
+}
+
+function numeroForMaiorOuMenorQueONumeroPermitido(numero){
+    return numero > maiorValor || numero < menorValor
+}
+
+
+document.body.addEventListener('click' , e => {
+    if(e.target.id == 'jogar-novamente'){
+        window.location.reload()
+    }
+})
